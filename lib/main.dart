8790e43e7309111223f9162de7623756b5f0fcb2
SethMegbetor <sethmegbetor@gmail.com>
2021-11-26 15:19:17 +0000

    end of class friday

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,60 @@
+// ignore_for_file: deprecated_member_use, avoid_print
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp( MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+   setState(() {
+      questionIndex += 1;
+   });
+    print(questionIndex);
+  }
+
 
   @override
   Widget build(BuildContext context) {
+  
+
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Center(child: Text('The title of my app')),
-            ),
-            body: Column(
-              children: const [
-                Text('the question'),
-                RaisedButton(
-                  child: Text('Answer 1'),
-                  onPressed: null,
-                ),
-                RaisedButton(
-                  child: Text('Answer 1'),
-                  onPressed: null,
-                ),
-                RaisedButton(
-                  child: Text('Answer 1'),
-                  onPressed: null,
-                )
-              ],
-            )));
+      appBar: AppBar(
+        title: const Center(child: Text('The title of my app')),
+      ),
+      body: Column(
+        children: [
+          Text(questions[questionIndex]),
+          RaisedButton(
+            child: const Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: const Text('Answer 1'),
+            onPressed: () => print('Answer 2 chosen'),
+          ),
+          RaisedButton(
+            child: const Text('Answer 1'),
+            onPressed: () {
+              print('Answer 3 chosen');
+            },
+          )
+        ],
+      ),
+    ));
   }
 }
