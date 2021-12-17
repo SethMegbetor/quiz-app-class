48a5e9cbba9b7841b93693d2c1e2599e43e9645e
“SethMegbetor” <sethmegbetor@gmail.com>
2021-12-17 10:51:51 +0000

    middle of class friday

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,20 +1,23 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectHandler;
   final String answerText;
-
 
   Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      // width: double.infinity,
+      width: 150,
+      height: 40,
+      margin: EdgeInsets.all(5),
       child: RaisedButton(
-        child: Text(answerText),
+        child: Text(answerText, style: const TextStyle(fontSize: 20),),
         textColor: Colors.white,
         color: Colors.blue,
         onPressed: selectHandler,
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,75 @@
 // ignore_for_file: deprecated_member_use, avoid_print
 
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
+  final _questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'red', 'score': 10},
+        {'text': 'blue', 'score': 5},
+        {'text': 'green', 'score': 3},
+        {'text': 'black', 'score': 1},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'cat', 'score': 10},
+        {'text': 'dog', 'score': 5},
+        {'text': 'pig', 'score': 3},
+        {'text': 'lion', 'score': 1},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite food?',
+      'answers': [
+        {'text': 'akple', 'score': 10},
+        {'text': 'kokonte', 'score': 5},
+        {'text': 'gari', 'score': 3},
+        {'text': 'yakayaka', 'score': 10},
+      ]
+    }
+  ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
     setState(() {
       _questionIndex += 1;
     });
     print(_questionIndex);
+
+    if (_questionIndex < _questions.length) {
+      print('We dont have any more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['blue', 'red', 'green', 'yellow']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['cat', 'dog', 'pig', 'cow']
-      },
-      {
-        'questionText': 'What\'s your favorite food?',
-        'answers': ['pizza', 'burger', 'sushi', 'ramen']
-      },
-    {
-        'questionText': 'What\'s your favorite sport?',
-        'answers': ['soccer', 'basketball', 'tennis', 'football']
-      },
-    ];
-
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Personality Quiz App')),
-      ),
-      body: Column(
-        children: [
-          Question(questions[_questionIndex]['questionText'] as String),
-        ...(questions[_questionIndex] ['answers']as List<String>).map((answer) {
-          return Answer(_answerQuestion, answer);
-        }).toList()
-        ],
-        
-    
-      ),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Center(child: Text('Personality Quiz App')),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questions: _questions,
+                  questionIndex: _questionIndex,
+                )
+              : const Result()),
+    );
   }
 }
