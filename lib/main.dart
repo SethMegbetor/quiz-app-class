eb800d1b0694e5aa908ee6cececb4381f2e4743f
“SethMegbetor” <sethmegbetor@gmail.com>
2021-12-17 14:22:16 +0000

    done with class project

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,16 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex += 1;
     });
@@ -69,7 +78,7 @@ class _MyAppState extends State<MyApp> {
                   questions: _questions,
                   questionIndex: _questionIndex,
                 )
-              : const Result()),
+              :  Result(_totalScore, _resetQuiz)),
     );
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key? key}) : super(key: key);
+  final int resultScore;
+  final VoidCallback resetHandler;
 
+  Result(this.resultScore, this.resetHandler);
+
+String get resultPhrase{
+  String resultText;
+  if(resultScore <= 8){
+    resultText = 'grade A you are awesome \n your total score is:  $resultScore';
+  }else if(resultScore <= 12){
+    resultText = 'Pretty likeable grade B \n your total score is:  $resultScore';
+  }else if(resultScore <= 16){
+    resultText = 'You are ... strange? grade C \n your total score is:  $resultScore';
+  }else{
+    resultText = 'You are so bad! grade D \n your total score is:  $resultScore';
+  }
+  return resultText;
+}
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('End of Quiz'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(resultPhrase,),
+          TextButton(
+            child: Text('Restart Quiz'),
+            style: TextButton.styleFrom(
+              primary: Colors.orange,
+            ),
+            onPressed: resetHandler,
+          )
+        ],
+      ),
     );
   }
 }
